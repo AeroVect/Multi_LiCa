9533ba977821a016fb1b583948c6684f39e6f1b2
Himadri Roy <163464943+himadriroyy@users.noreply.github.com>
2024-09-17 15:58:02 -0400

    changes to setup.py (#8)

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 import os
 from glob import glob
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = "multi_lidar_calibrator"
-submodules = "multi_lidar_calibrator/calibration"
 
 setup(
     name=package_name,
     version="0.0.1",
-    packages=[package_name, submodules],
+    packages= find_packages(exclude=["test", "doc"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
